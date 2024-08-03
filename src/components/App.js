9268a3d5e7db6bb3d8f9150072c818fc304ac09c
sandip <mr.sandippradhan@gmail.com>
2024-08-03 20:35:20 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react'
+import Mark1 from './Mark1'
+import Mark2 from './Mark2'
 
+let App= ()=>{
+    return (
+        <div className='app'>
+            <Mark1/>
+            {/* <Mark2 /> */}
+        </div>
+    )
+}
+export default App
